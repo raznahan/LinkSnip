12633889b858e59af56b3d54526b3d9aa68a3ccf
Moji Raznahan <moji.rzn@gmail.com>
2025-02-28 13:40:56 -0800

    chore(backend): setup Node.js with TypeScript, Express, and initial folder structure

---
+++ backend/src/index.ts
@@ -0,0 +1,20 @@
+import express from  'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3004;
+app.use(cors());
+app.use(express.json());
+
+
+//basic route to check if servier is running
+app.get('/', (req:any, res:any) => {
+    res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT ${PORT}`);
+});
